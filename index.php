d2d4df2e427cb5cefd6ef2ef67337da7d27467c7
luisbaltazar2020 <BALTAZAR.GARCIA@ALUMNOS.UDG.MX>
2021-11-16 19:05:14 -0600

    login plano

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <html>
     <head>
-        Grupos alimenticios
+        <link rel="stylesheet" href="./css/index.css">
     </head>
     <body>
-        
+    <div class="titulo">Grupos Alimenticios, S.A de C.V</div>
+        <div class="login">
+            <img src="https://definicion.de/wp-content/uploads/2019/06/perfildeusuario.jpg"/><br><br>
+            Usuario:<br>
+            <input class="usuario" type="text"></input> <br><br>
+            Password: <br>
+            <input class="pass" type="password"></input><br><br>
+            <button class="inicio" >Iniciar sesion</button>
+        </div>
     </body>
 </html>

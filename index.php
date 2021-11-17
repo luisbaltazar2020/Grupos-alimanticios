b320075a86d47303ad050de7275c429ab5252a32
luisbaltazar2020 <BALTAZAR.GARCIA@ALUMNOS.UDG.MX>
2021-11-16 19:21:32 -0600

    primera funcion js

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <html>
     <head>
         <link rel="stylesheet" href="./css/index.css">
+        <script src="JS/funciones.js"></script>
     </head>
     <body>
     <div class="titulo">Grupos Alimenticios, S.A de C.V</div>
         <div class="login">
             <img src="https://definicion.de/wp-content/uploads/2019/06/perfildeusuario.jpg"/><br><br>
-            Usuario:<br>
-            <input class="usuario" type="text"></input> <br><br>
-            Password: <br>
-            <input class="pass" type="password"></input><br><br>
-            <button class="inicio" >Iniciar sesion</button>
+            <form name="forma01">
+                Usuario:<br>
+                <input id="usuario" type="text" name="usuario"></input> <br><br>
+                Password: <br>
+                <input id="pass" type="password" name="pass"></input><br>
+            </form>
+            <button type="submit" class="inicio" onclick="validacion();">Iniciar sesion</button>
         </div>
     </body>
 </html>
